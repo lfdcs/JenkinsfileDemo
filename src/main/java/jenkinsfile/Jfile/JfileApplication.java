4f472439f45db89239d727416b63e03818de2d95
student <eliayves@gmail.com>
2020-11-27 19:11:21 +0000

    Jfile

---
+++ src/main/java/jenkinsfile/Jfile/JfileApplication.java
@@ -0,0 +1,13 @@
+package jenkinsfile.Jfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfileApplication.class, args);
+	}
+
+}
